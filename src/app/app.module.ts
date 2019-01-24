2fa1f5c8184b56c8781a9178e849d2c238c9d8ed
RAHUL VISHWAKARMA <33103549+rahulvish1364@users.noreply.github.com>
2019-01-24 13:40:10 +0530

    Page updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import {EventModule} from './event/event.module';
+
+
+////////////////////////////Components////////////////////////////
 import { AppComponent } from './app.component';
 import {IndexComponent} from './index/index.component'
 import {FooterComponent} from  './index/footer/footer.component';
@@ -11,7 +15,8 @@ import { from } from 'rxjs';
 import { SocialLoginModule , AuthService, GoogleLoginProvider , FacebookLoginProvider , LinkedInResponse, AuthServiceConfig, LinkedinLoginProvider}from 'ng4-social-login';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-import {EntitiesModule} from './entities/entities.module'
+import {EntitiesModule} from './entities/entities.module';
+import {  BirthdayComponent} from "./birthday/birthday.component";
 const config = new AuthServiceConfig([
  {
    id: GoogleLoginProvider.PROVIDER_ID,
@@ -42,12 +47,14 @@ export function provideConfig(){
     LoginCompComponent,
     CartComponent,
     CheckoutComponent,
+    BirthdayComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SocialLoginModule,
-    EntitiesModule
+    EntitiesModule,
+    EventModule
   ],
   providers: [{provide :AuthServiceConfig, useFactory: provideConfig}],
   bootstrap: [AppComponent]
